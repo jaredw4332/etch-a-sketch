c8b63c4af651dced5e7bde7c4107d9d25336710b
Jared <jared.wilson4332@gmail.com>
2022-04-09 21:51:17 -0600

    add reset button

--- javascript.js
+++ javascript.js
@@ -7,12 +7,24 @@ const gridContainer = document.querySelector('.gridContainer')
 for(let i = 0; i < 256; i++){
     let gridPiece = document.createElement('div')
     gridPiece.classList.add('grid')
-    gridPiece.textContent = 'gun'
     gridPiece.addEventListener('mouseover', gridMouseOver, true)
     gridContainer.appendChild(gridPiece)
 
 
     function gridMouseOver(){
         gridPiece.classList.add('gridMouseOver')
+    }
+}
+
+let resetButton = document.createElement('button')
+resetButton.addEventListener('click', resetGrid)
+resetButton.textContent = 'Clear'
+document.body.appendChild(resetButton)
+
+function resetGrid(){
+    let gridColored = document.querySelectorAll('.gridMouseOver')
+    for (let i = 0; i < gridColored.length; i++){
+        gridColored[i].classList.remove('gridMouseOver')
+
     }
 }
